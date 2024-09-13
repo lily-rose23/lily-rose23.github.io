b51f0f2b99103854615dd8fec519d7ee5c598965
lily-rose23 <zaheerhuma000@gmail.com>
2024-09-13 16:13:06 +0100

    new commits

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const images = document.querySelectorAll("img");
 
 for(const image of images){
-   fetch ("https://dog.ceo/api/breeds/image/random")
+   fetch ("")
    .then(response => response.json())
    .then(data => {
       image.src = data.message
